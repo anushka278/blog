c19577a92d138c99c55789df8ccff7fe64e15c76
Anushka Rawat <anushkarawat@mac.lan>
2025-06-20 18:49:07 -0700

    update LinkedIn URL to correct profile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -221,7 +221,7 @@ const Home = () => {
                 </svg>
                 Get In Touch
               </a>
-              <a href="https://linkedin.com/in/amitkaistha" className="btn btn-secondary btn-large">
+                              <a href="https://www.linkedin.com/in/amit-kaistha/" className="btn btn-secondary btn-large">
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                   <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
                   <rect x="2" y="9" width="4" height="12"/>
